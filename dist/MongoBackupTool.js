18c3e0704d8a472d0f23283130646b1c9e575be3
John Lyon-Smith <john@lyon-smith.org>
2018-01-11 08:26:59 -0800

    Working tool with Slack integration

--- dist/MongoBackupTool.js
+++ dist/MongoBackupTool.js
@@ -27,6 +27,10 @@ var _temp = require('temp');
 
 var _temp2 = _interopRequireDefault(_temp);
 
+var _fs = require('fs');
+
+var _fs2 = _interopRequireDefault(_fs);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 class MongoBackupTool {
@@ -34,9 +38,34 @@ class MongoBackupTool {
     this.log = log;
   }
 
+  static findTool(name, paths) {
+    let foundPath = null;
+
+    for (let path of paths) {
+      let fullPath = path.join(path, name);
+
+      if (_fs2.default.existsSync(fullPath)) {
+        foundPath = fullPath;
+        break;
+      }
+    }
+
+    if (!foundPath) {
+      throw new Error(`Unable to find tool '${name}'`);
+    }
+  }
+
   async run(argv) {
     const options = {
-      boolean: ['help', 'version']
+      boolean: ['help', 'version'],
+      string: ['profile', 'bucket', 'max', 'mongo', 'api', 'channel'],
+      alias: {
+        'p': 'profile',
+        'b': 'bucket',
+        'n': 'max',
+        't': 'api',
+        'c': 'channel'
+      }
     };
     this.args = (0, _minimist2.default)(argv, options);
 
@@ -51,12 +80,169 @@ class MongoBackupTool {
       this.log.info(`
 usage: mongo-backup [options]
 
+description:
+  Backup a MongoDB database to an Amazon S3 bucket, delete older archives,
+  and notify a Slack channel of status.
+
 options:
-  --help                        Shows this help.
-  --version                     Shows the tool version.
+  [--profile | -p] PROFILE        AWS profile
+  [--bucket | -b] BUCKET_NAME     AWS bucket
+  [--max | -n] MAX_BACKUPS        Maximum number of backups to keep
+  [--mongo | -m] MONGO_URI        Full URI of MongoDB to backup
+  [--api | -t] SLACK_API_TOKEN    Slack API token
+  [--channel | -c] SLACK_CHANNEL  Slack channel
+  --help                          Shows this help
+  --version                       Shows the tool version
 `);
       return 0;
     }
+
+    // main do
+    //   profile = options[:profile]
+    //   bucket_name = options[:bucket]
+    //   max_backups = options[:max].to_i
+    //   mongo_path = options[:mongo]
+    //   slack_api_token = options[:api]
+    //   slack_channel = options[:channel]
+    //
+    //   if profile.nil?
+    //     exit_now! "Must specify AWS profile"
+    //   end
+    //
+    //   if bucket_name.nil?
+    //     exit_now! "Must specify AWS bucket name"
+    //   end
+    //
+    //   if max_backups.nil? or max_backups == 0
+    //     max_backups = 20
+    //     puts "Max backups is #{max_backups}"
+    //   end
+    //
+    //   if mongo_path.nil?
+    //     exit_now! "Must specify a database to backup!"
+    //   end
+    //
+    //   if slack_api_token.nil? or slack_channel.nil?
+    //     puts "WARNING: No Slack API and/or channel token given, no notification will be sent"
+    //   end
+    //
+    //   mongo_uri = Mongo::URI.new(mongo_path)
+    //   database = mongo_uri.database
+    //   user = mongo_uri.credentials[:user]
+    //   password = mongo_uri.credentials[:password]
+    //   mongo_server = mongo_uri.servers[0]
+    //
+    //   mongodump_tool = find_tool('mongodump', ['/usr/bin', '/usr/local/bin'])
+    //   aws_tool = find_tool('aws', ['/usr/bin', '/usr/local/bin'])
+    //   scutil_tool = find_tool('scutil', ['/usr/sbin'])
+    //   tar_tool = find_tool('tar', ['/usr/bin'])
+    //
+    //   backup_status = :failed
+    //
+    //   Dir.mktmpdir do |dump_dir_name|
+    //     begin
+    //       local_host_name = `#{scutil_tool} --get LocalHostName`.strip
+    //
+    //       puts "Backing up mongodb://#{mongo_server}/#{database} from #{local_host_name}"
+    //
+    //       if !(user.nil? and password.nil?)
+    //         `#{mongodump_tool} -h #{mongo_server} -u #{user} -p #{password} -d #{database} -o #{dump_dir_name}`
+    //       else
+    //         `#{mongodump_tool} -h #{mongo_server} -d #{database} -o #{dump_dir_name}`
+    //       end
+    //
+    //       if $? != 0
+    //         raise "Unable to create dump of mongodb://#{mongo_server}/#{database}"
+    //       end
+    //
+    //       date_time = DateTime.now.strftime("%Y%m%d-%H%M%SZ")
+    //
+    //       backup_filename = "#{local_host_name}-#{database}-#{date_time}.tar.gz"
+    //
+    //       `cd #{dump_dir_name}; #{tar_tool} -czvf #{backup_filename} #{database}/*`
+    //
+    //       if $? != 0
+    //         raise "Unable to create tar zip file '#{backup_filename}'"
+    //       end
+    //
+    //       `cd #{dump_dir_name}; #{aws_tool} s3 cp #{backup_filename} s3://#{bucket_name}/ --profile #{profile}`
+    //
+    //       if $? != 0
+    //         raise "Unable to upload '#{backup_filename}' to s3://#{bucket_name}/"
+    //       end
+    //
+    //       # Reduce backups to desired maximum
+    //       backup_filenames = `#{aws_tool} s3 ls s3://#{bucket_name} --profile #{profile}`.split('\n')
+    //
+    //       if $? != 0
+    //         raise "Unable to get list of existing backups from s3://#{bucket_name}"
+    //       end
+    //
+    //       # backup_filenames = %w(
+    //       #   'host-database-20160624-002055Z.tar.gz',
+    //       #   'host-database-20160620-002055Z.tar.gz',
+    //       #   'host-database-20160621-002055Z.tar.gz',
+    //       #   'host-database-20160708-002055Z.tar.gz',
+    //       #   'host-database-20160601-002055Z.tar.gz.save',
+    //       # )
+    //       backup_filenames.sort!
+    //       backup_filenames.delete_if { |f| f.match(/save/) }
+    //       num_backups_to_delete = backup_filenames.count - max_backups
+    //
+    //       if num_backups_to_delete > 0
+    //         (0...num_backups_to_delete).each { |i|
+    //           `#{aws_tool} s3 rm s3://#{bucket_name}/#{backup_filenames[i]} --profile #{profile}`
+    //
+    //           if $? != 0
+    //             puts "WARNING: Unable to delete s3://#{bucket_name}/#{backup_filenames[i]}"
+    //           end
+    //         }
+    //       end
+    //
+    //       backup_status = :succeeded
+    //     ensure
+    //       Slack.configure do |config|
+    //         config.token = slack_api_token
+    //       end
+    //
+    //       unless slack_api_token.nil? or slack_channel.nil?
+    //         if backup_status == :succeeded
+    //           message = "MongoDB backup complete"
+    //           attachments = [
+    //             {
+    //                 "color": "good",
+    //                 "title": "MongoDB Backup Succeeded",
+    //                 "text": "A backup of database mongodb://#{mongo_server}/#{database} was made from #{local_host_name} to s3://#{bucket_name}/#{backup_filename}",
+    //                 "ts": Time.now.utc.to_i
+    //             }
+    //           ]
+    //         else
+    //           message = "MongoDB backup failed"
+    //           attachments = [
+    //               {
+    //                   "color": "danger",
+    //                   "title": "MongoDB Backup Failed",
+    //                   "text": "A backup of database mongodb://#{mongo_server}/#{database} from #{local_host_name} failed.",
+    //                   "ts": Time.now.utc.to_i
+    //               }
+    //           ]
+    //         end
+    //         begin
+    //           slack = Slack::Web::Client.new
+    //           slack.chat_postMessage(channel: slack_channel, text: message, attachments: attachments, as_user: true)
+    //         rescue
+    //           puts "Unable to send Slack message to #{slack_channel}"
+    //         end
+    //       end
+    //     end
+    //   end
+    //
+    //   if backup_status == :succeeded
+    //     puts "Backup was successful"
+    //   else
+    //     puts "Backup failed"
+    //   end
+    // end
 
     return 0;
   }
--- src/MongoBackupTool.js
+++ src/MongoBackupTool.js
@@ -4,35 +4,244 @@ import util from 'util'
 import path from 'path'
 import process from 'process'
 import temp from 'temp'
+import fs from 'fs'
+import mongo from 'mongodb'
+import mongoUri from 'mongodb-uri'
+import { execSync } from 'child_process'
+import moment from 'moment'
+import { WebClient } from '@slack/client'
 
 export class MongoBackupTool {
   constructor(log) {
     this.log = log
   }
 
+  static findTool(name, dirs) {
+    let foundPath = null
+
+    for (let dir of dirs) {
+      const toolPath = path.join(dir, name)
+
+      if (fs.existsSync(toolPath)) {
+        foundPath = toolPath
+        break
+      }
+    }
+
+    if (!foundPath) {
+      throw new Error(`Unable to find tool '${name}'`)
+    } else {
+      return foundPath
+    }
+  }
+
+  async doBackup() {
+    const mkdirAsync = util.promisify(temp.mkdir)
+    const dumpDirName = await mkdirAsync('mongo-backup-')
+
+    this.log.info(`Backing up '${mongoUri.format(this.uri)}' from '${this.localHost}'`)
+
+    try {
+      if (this.uri.user && this.uri.password) {
+        execSync(`${this.tools.mongodump} -h ${this.uri.fullHost} -u ${this.uri.user} -p ${this.uri.password} -d ${this.uri.database} -o ${dumpDirName}`)
+      } else {
+        execSync(`${this.tools.mongodump} -h ${this.uri.fullHost} -d ${this.uri.database} -o ${dumpDirName}`)
+      }
+    } catch (e) {
+      this.log.error(`Unable to create dump of '${mongoUri.format(this.uri)}'. ${e.message}`)
+      return false
+    }
+
+    const dateTime = moment().format('YYMMDD-hhmmss') + 'Z'
+
+    this.backupFilename = `${this.localHost}-${this.uri.database}-${dateTime}.tar.gz`
+
+    try {
+      execSync(`${this.tools.tar} -czvf ${this.backupFilename} ${this.uri.database}/*`, { cwd: dumpDirName })
+    } catch (e) {
+      this.log.error(`Unable to tar file '${this.backupFilename}'. ${e.message}`)
+      return false
+    }
+
+    try {
+      execSync(`${this.tools.aws} s3 cp ${this.backupFilename} s3://${this.args.bucket}/ --profile ${this.args.profile}`, { cwd: dumpDirName })
+    } catch (e) {
+      this.log.error(`Unable to upload '${this.backupFilename}' to s3://${this.args.bucket}/. ${e.message}`)
+      return false
+    }
+
+    let backupFilenames = []
+
+    try {
+      backupFilenames = execSync(`${this.tools.aws} s3 ls s3://${this.args.bucket} --profile ${this.args.profile}`).toString().split('\n')
+    } catch (e) {
+      this.log.error(`Unable to get list of existing backups from s3://${this.args.bucket}. ${e.message}`)
+      return false
+    }
+
+    backupFilenames = backupFilenames.filter(f => f.startsWith(this.localHost) && !f.endsWith('.save') && f.trim().length > 0)
+    backupFilenames.sort() // In-place sort
+
+    let numBackupsToDelete = Math.max(0, backupFilenames.length - this.args.max)
+
+    if (numBackupsToDelete > 0) {
+      for (let i = 0; i < numBackupsToDelete; i++) {
+        try {
+          execSync(`${this.tools.aws} s3 rm s3://${this.args.bucket}/${backupFilenames[i]} --profile ${this.args.profile}`)
+        } catch (e) {
+          this.log.warning(`Unable to delete s3://${this.args.bucket}/${backupFilenames[i]}`)
+        }
+      }
+    }
+
+    return true
+  }
+
   async run(argv) {
     const options = {
       boolean: [ 'help', 'version' ],
+      string: [ 'profile', 'bucket', 'max', 'mongo', 'slack', 'channel' ],
+      alias: {
+        'p': 'profile',
+        'b': 'bucket',
+        'n': 'max',
+        't': 'slack',
+        'c': 'channel',
+        'm': 'mongo'
+      },
+      default: {
+        'max': '10'
+      }
     }
     this.args = parseArgs(argv, options)
-
-    const command = this.args._[0]
 
     if (this.args.version) {
       this.log.info(`${fullVersion}`)
       return 0
     }
 
-    if (this.args.help || !command) {
+    if (this.args.help) {
       this.log.info(`
 usage: mongo-backup [options]
 
+description:
+  Backup a MongoDB database to an Amazon S3 bucket, delete older archives,
+  and notify a Slack channel of status.
+
 options:
-  --help                        Shows this help.
-  --version                     Shows the tool version.
+  --profile, -p     AWS profile name
+  --bucket, -b      AWS bucket name
+  --max, -n         Maximum number of backups to keep
+  --mongo, -m       Full URI of MongoDB to backup
+  --slack, -t       Slack API token
+  --channel, -c     Slack channel name (without the # prefix)
+  --help            Shows this help
+  --version         Shows the tool version
 `)
       return 0
     }
+
+    if (!this.args.profile) {
+      this.log.error('Must specify an AWS profile')
+      return false
+    }
+
+    if (!this.args.bucket) {
+      this.log.error('Must specify AWS bucket name')
+      return false
+    }
+
+    this.args.max = parseInt(this.args.max)
+
+    if (this.args.max <= 0) {
+      this.log.error('Max backups must be greater than zero')
+      return false
+    }
+
+    if (!this.args.mongo) {
+      this.log.error('Must specify a database to backup')
+      return false
+    }
+
+    this.log.info(`Max backups is ${this.args.max}`)
+
+    this.uri = null
+
+    try {
+      this.uri = mongoUri.parse(this.args.mongo)
+    } catch (e) {
+      this.log.error(`Unable to parse MongoDB URI. ${e.message}`)
+      return false
+    }
+
+    const findTool = this.constructor.findTool
+    this.tools = {}
+
+    try {
+      this.tools = {
+        mongodump: findTool('mongodump', ['/usr/bin', '/usr/local/bin']),
+        aws: findTool('aws', ['/usr/bin', '/usr/local/bin']),
+        scutil: findTool('scutil', ['/usr/sbin']),
+        tar: findTool('tar', ['/usr/bin']),
+      }
+    } catch (e) {
+      this.log.error(e.message)
+      return false
+    }
+
+    this.uri.host = this.uri.hosts[0].host
+    this.uri.port = this.uri.hosts[0].port
+    this.uri.fullHost = this.uri.host + (this.uri.port ? ':' + this.uri.port : '')
+
+    this.localHost = ''
+
+    try {
+      this.localHost = execSync(`${this.tools.scutil} --get LocalHostName`).toString().trim()
+    } catch (e) {
+      this.log.error('Unable to retrieve local host name')
+      return -1
+    }
+
+    let succeeded = await this.doBackup()
+    let message = ''
+    let attachments = {}
+
+    if (succeeded) {
+      message = 'MongoDB backup complete'
+      attachments = [
+        {
+            color: "good",
+            title: "MongoDB Backup Succeeded",
+            ts: moment.utc().unix(),
+            text: `A backup of database mongodb://${this.uri.fullHost}/${this.uri.database} was made from ${this.localHost} to s3://${this.args.bucket}/${this.backupFilename}`,
+        }
+      ]
+    } else {
+      message = "MongoDB backup failed"
+      attachments = [
+          {
+              color: 'danger',
+              title: 'MongoDB Backup Failed',
+              ts: moment.utc().unix(),
+              text: `A backup of database mongodb://${this.uri.fullHost}/${this.uri.database} from ${this.localHost} failed.`,
+          }
+      ]
+    }
+
+    if (!this.args.slack || !this.args.channel) {
+      this.log.warning('No Slack API and/or channel token given; no notifications will be sent')
+    } else {
+      const webClient = new WebClient(this.args.slack)
+
+      // See http://slackapi.github.io/node-slack-sdk/reference/ChatFacet#ChatFacet+postMessage
+      try {
+        await webClient.chat.postMessage(this.args.channel, message, { attachments, as_user: true })
+      } catch (e) {
+        this.log.warning(`Unable to send Slack message to ${this.args.channel}. ${e.message}`)
+      }
+    }
+
+    this.log.info(succeeded ? 'Backup successful' : 'Backup failed')
 
     return 0
   }
